d338da54bee5213cce8966146017abb31ac32b51
akash1by137 <thisisakashsrivastava@gmail.com>
2025-01-13 14:05:53 +0530

    Added Logic 3 for LATAM

--- XX_EMP_PKG_1.sql
+++ XX_EMP_PKG_1.sql
@@ -3,5 +3,6 @@ DECLARE
 BEGIN 
 dbms_output.put_line ('This is Logic 1 for the XX_EMP_PKG_1 program for US');
 dbms_output.put_line ('Added Logic 2 for the XX_EMP_PKG_1 program for US');
+dbms_output.put_line ('Added Logic 3 for the XX_EMP_PKG_1 program for LATAM');
 END;
 --==============================================================================================================================
