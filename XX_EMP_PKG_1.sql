f44b60e52af87325508a11ff3fe6fb4ae9a297b7
akash1by137 <thisisakashsrivastava@gmail.com>
2025-01-13 13:58:23 +0530

    Added Logic 2 in file XX_EMP_PKG_1.sql

--- XX_EMP_PKG_1.sql
+++ XX_EMP_PKG_1.sql
@@ -1,6 +1,7 @@
 --==============================================================================================================================
 DECLARE
 BEGIN 
-dbms_output.put_line ('This is LOgic 1 for the XX_EMP_PKG_1 program');
+dbms_output.put_line ('This is Logic 1 for the XX_EMP_PKG_1 program for US');
+dbms_output.put_line ('Added Logic 2 for the XX_EMP_PKG_1 program for US');
 END;
 --==============================================================================================================================
