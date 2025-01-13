9020f83421f952296615bb7b616494adfff12099
akash1by137 <thisisakashsrivastava@gmail.com>
2025-01-13 14:23:21 +0530

    Added Logic 3 for LATAM

--- XX_EMP_PKG_1.sql
+++ XX_EMP_PKG_1.sql
@@ -3,6 +3,6 @@ DECLARE
 BEGIN 
 dbms_output.put_line ('This is Logic 1 for the XX_EMP_PKG_1 program for US');
 dbms_output.put_line ('Added Logic 2 for the XX_EMP_PKG_1 program for US');
-dbms_output.put_line ('Added Logic 3 for the XX_EMP_PKG_1 program for LATAM');
+dbms_output.put_line ('Update Logic 3 for the XX_EMP_PKG_1 program to cater both LATAM and Chile SP LATAM');
 END;
 --==============================================================================================================================
